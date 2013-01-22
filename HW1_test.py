ae7e859fd280ff8b95905ffc72ad9c7d4425f429
Won Steinbach <wonstein@gmail.com>
2013-01-22 10:07:28 -0500

    Described bugs in test file

--- HW1_test.py
+++ HW1_test.py
@@ -15,6 +15,7 @@ class TestHW1(unittest.TestCase):
 		
 	def test_shout3(self):
 		self.assertEqual(HW1.shout("Blah!"), "BLAH!")
+		# Bug: prints an extra "!"
 	
 	def test_shout4(self):
 		self.assertEqual(HW1.shout("Hey. You!"), "HEY! YOU!")
@@ -24,12 +25,14 @@ class TestHW1(unittest.TestCase):
 
 	def test_reversewords1(self):
 		self.assertEqual(HW1.reversewords("Hi, my name is Blah."), "Blah is name my, Hi.")
+		# Bug: "Prints too many commas"
 	
 	def test_reversewordletters1(self):
 		self.assertEqual(HW1.reversewordletters("Hi, my name is Blah."), "iH, ym, eman, si, halB")
 		
 	def test_piglatin1(self):
 		self.assertEqual(HW1.piglatin("nix"), "ixnay")
+		# Bug: doesn't work when first vowel is second to last letter in word
 
 if __name__ == '__main__':
 	unittest.main()
